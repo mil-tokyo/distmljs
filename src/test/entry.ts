b433a813b330f3e75c40ee8e6a659667f6e11719
Masatoshi Hidaka <hidaka@edgeintelligence.jp>
2022-02-04 11:37:11 +0900

    improve test ui

--- src/test/entry.ts
+++ src/test/entry.ts
@@ -12,8 +12,55 @@ XXX: テスト対象グループ名、YYY: テスト対象クラス名
 import './tensor/index';
 import './nn/index';
 import { initializeNNWebGLContext } from '../tensor';
+import {
+  AllTestTargets,
+  saveTestFlagAndReload,
+  testFlag,
+  TestFlag,
+  TestTarget,
+} from './testFlag';
+
+function makeTargetList() {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const tl = document.getElementById('target-list')!;
+  const currentTarget = testFlag;
+  for (const name of AllTestTargets) {
+    const label = document.createElement('label');
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    check.name = name;
+    if (currentTarget[name]) {
+      check.checked = true;
+    }
+    label.appendChild(check);
+    label.appendChild(document.createTextNode(name));
+    tl.appendChild(label);
+  }
+  const submit = document.createElement('button');
+  submit.type = 'button';
+  submit.onclick = () => {
+    const flag: TestFlag = {};
+    const qs = document.querySelectorAll('#target-list input:checked');
+    for (let i = 0; i < qs.length; i++) {
+      const check = qs[i] as HTMLInputElement;
+      flag[check.name as TestTarget] = true;
+    }
+    saveTestFlagAndReload(flag);
+  };
+  submit.innerText = 'Select and reload';
+  tl.appendChild(submit);
+}
 
 window.addEventListener('load', async () => {
-  await initializeNNWebGLContext();
+  makeTargetList();
+  if (testFlag.webgl) {
+    try {
+      await initializeNNWebGLContext();
+    } catch (error) {
+      alert(
+        `Failed to initialize WebGL. Uncheck webgl in target selection. ${error}`
+      );
+    }
+  }
   mocha.run();
 });
