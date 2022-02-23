c52ee54f6ce6d77c8a57b2b17fd69949dbc5869c
Masatoshi Hidaka <hidaka@edgeintelligence.jp>
2022-02-23 18:04:17 +0900

    文法の改善

--- src/tensor/cpu/core/manipulation.ts
+++ src/tensor/cpu/core/manipulation.ts
@@ -1,23 +1,33 @@
 import { TypedArrayTypes } from '../../../dtype';
+import { arraySum } from '../../../util';
 import { CPUTensor } from '../cpuTensor';
 
-function repeat_sub(
+function repeatSub(
   dx: TypedArrayTypes,
   xShape: ReadonlyArray<number>,
   xStrides: ReadonlyArray<number>,
   repeats: ReadonlyArray<number>,
   axis: number
-): Array<any> {
-  let dy: Array<any> = [];
-  if(axis==0){
-    for(let i=0; i<xShape[0]; ++i){
-      for(let j=0; j<repeats[i]; ++j){
-        dy = [...dy, ...dx.slice(i*xStrides[0], (i+1)*xStrides[0])];
+): number[] {
+  let dy: number[] = [];
+  if (axis === 0) {
+    for (let i = 0; i < xShape[0]; ++i) {
+      for (let j = 0; j < repeats[i]; ++j) {
+        dy = [...dy, ...dx.slice(i * xStrides[0], (i + 1) * xStrides[0])];
       }
     }
-  }else{
-    for(let i=0; i<xShape[0]; ++i){
-      dy = [...dy, ...repeat_sub(dx.slice(i*xStrides[0], (i+1)*xStrides[0]), xShape.slice(1), xStrides.slice(1), repeats, axis-1)];
+  } else {
+    for (let i = 0; i < xShape[0]; ++i) {
+      dy = [
+        ...dy,
+        ...repeatSub(
+          dx.slice(i * xStrides[0], (i + 1) * xStrides[0]),
+          xShape.slice(1),
+          xStrides.slice(1),
+          repeats,
+          axis - 1
+        ),
+      ];
     }
   }
   return dy;
@@ -28,65 +38,68 @@ export function repeat(
   repeats: ReadonlyArray<number> | number,
   axis?: number
 ): CPUTensor {
-  if(axis==undefined){  //軸指定がないとき
-    if(x.ndim==0){  //xがスカラーのとき
-      const y = CPUTensor.zeros([repeats as number], x.dtype);
+  if (axis == undefined) {
+    //軸指定がないとき
+    if (typeof repeats !== 'number') {
+      throw new Error('repeat: repeats must be number when axis === undefined');
+    }
+    if (x.ndim === 0) {
+      //xがスカラーのとき
+      const y = CPUTensor.zeros([repeats], x.dtype);
       const dx = x.getBuffer().data;
       const dy = y.getBuffer().data;
-      for(let i=0; i<(repeats as number); ++i){
+      for (let i = 0; i < repeats; ++i) {
         dy[i] = dx[0];
       }
       return y;
-    }else{  //xが多次元配列のとき
-      const y = CPUTensor.zeros([x.getBuffer().length*(repeats as number)], x.dtype);
+    } else {
+      //xが多次元配列のとき
+      const y = CPUTensor.zeros([x.getBuffer().length * repeats], x.dtype);
       const dx = x.getBuffer().data;
       const dy = y.getBuffer().data;
-      for(let i=0; i<x.getBuffer().length; ++i){
-        for(let j=0; j<(repeats as number); ++j){
-          dy[i*(repeats as number)+j] = dx[i];
+      for (let i = 0; i < x.getBuffer().length; ++i) {
+        for (let j = 0; j < repeats; ++j) {
+          dy[i * repeats + j] = dx[i];
         }
       }
       return y;
     }
-  }else{  //軸指定があるとき
-    if(typeof repeats === 'number'){ //repeatsがnumber型のとき
-      const y_shape: number[] = [];
-      for(let i=0; i<x.shape.length; ++i){
-        if(i==axis){
-          y_shape[i] = x.shape[i]*(repeats as number);
-        }else{
-          y_shape[i] = x.shape[i];
+  } else {
+    //軸指定があるとき
+    if (typeof repeats === 'number') {
+      //repeatsがnumber型のとき
+      const yShape: number[] = [];
+      for (let i = 0; i < x.shape.length; ++i) {
+        if (i === axis) {
+          yShape[i] = x.shape[i] * repeats;
+        } else {
+          yShape[i] = x.shape[i];
         }
       }
       const dx = x.getBuffer().data;
-      let new_repeats: number[];
-      new_repeats = [];
-      for(let i=0; i<x.shape[axis]; ++i){
-        new_repeats = [...new_repeats, (repeats as number)];
+      const newRepeats: number[] = [];
+      for (let i = 0; i < x.shape[axis]; ++i) {
+        newRepeats.push(repeats);
       }
-      const dy = repeat_sub(dx, x.shape, x.strides, new_repeats, axis);
-      const y = CPUTensor.fromArray(dy, y_shape, x.dtype);
+      const dy = repeatSub(dx, x.shape, x.strides, newRepeats, axis);
+      const y = CPUTensor.fromArray(dy, yShape, x.dtype);
       return y;
-    }else{ //repeatsが配列の時
-      const y_shape: number[] = [];
-      for(let i=0; i<x.shape.length; ++i){
-        if(i==axis){
-          let sum = 0;
-          for(const e of (repeats as ReadonlyArray<number>)){
-            sum += e;
-          }
-          y_shape[i] = sum;
-        }else{
-          y_shape[i] = x.shape[i];
+    } else {
+      //repeatsが配列の時
+      const yShape: number[] = [];
+      for (let i = 0; i < x.shape.length; ++i) {
+        if (i === axis) {
+          yShape[i] = arraySum(repeats);
+        } else {
+          yShape[i] = x.shape[i];
         }
       }
       const dx = x.getBuffer().data;
-      const dy = repeat_sub(dx, x.shape, x.strides, (repeats as ReadonlyArray<number>), axis);
-      const y = CPUTensor.fromArray(dy, y_shape, x.dtype);
+      const dy = repeatSub(dx, x.shape, x.strides, repeats, axis);
+      const y = CPUTensor.fromArray(dy, yShape, x.dtype);
       return y;
     }
   }
-  throw new Error();
 }
 
 export function tile(
